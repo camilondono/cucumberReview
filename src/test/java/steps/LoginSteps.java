199c56e48473bab9ca70ef65db43d4076030dc04
camilondono <camilondono1216@gmail.com>
2022-12-09 11:06:53 -0500

    browser log in method update

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,13 +3,39 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import utils.ConfigReader;
+import utils.Constants;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginSteps {
+    public static WebDriver driver;
 
     @Given("User Navigates To the URL")
     public void user_navigates_to_the_url() {
+        ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        switch (ConfigReader.getPropertyValue("browser")){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("Invalid browser name");
 
-        System.out.println("1");
+        }
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+
+
     }
     @When("User Enters The correct UserName {string}")
     public void user_enters_the_correct_user_name(String username) {
