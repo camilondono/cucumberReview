c369ed42242a7133fe7d02def1446284e931d25b
camilondono <camilondono1216@gmail.com>
2022-12-09 10:55:55 -0500

    firts commit review framework

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,35 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+    @Given("User Navigates To the URL")
+    public void user_navigates_to_the_url() {
+
+        System.out.println("1");
+    }
+    @When("User Enters The correct UserName {string}")
+    public void user_enters_the_correct_user_name(String username) {
+
+        System.out.println(username);
+    }
+    @When("User Enters The correct Password {string}")
+    public void user_enters_the_correct_password(String password) {
+
+        System.out.println(password);
+    }
+    @When("user clicks On login button")
+    public void user_clicks_on_login_button() {
+
+        System.out.println("4");
+    }
+    @Then("user is logged in")
+    public void user_is_logged_in() {
+
+        System.out.println("5");
+    }
+
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,7 @@
+Feature: Test The Login Functionality
+  Scenario: The User Is Able To Login
+    Given User Navigates To the URL
+    When User Enters The correct UserName "Admin"
+    And User Enters The correct Password "Hum@nhrm123"
+    And user clicks On login button
+    Then user is logged in
