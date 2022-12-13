2c22c5272f43f24b839b74004288bd6f47d85558
camilondono <camilondono1216@gmail.com>
2022-12-12 22:14:53 -0500

    2nd review

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,61 +1,87 @@
 package steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.LoginPage;
+import utils.CommonMethods;
 import utils.ConfigReader;
 import utils.Constants;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class LoginSteps {
+public class LoginSteps extends CommonMethods {
     public static WebDriver driver;
 
     @Given("User Navigates To the URL")
     public void user_navigates_to_the_url() {
-        ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
-        switch (ConfigReader.getPropertyValue("browser")){
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            default:
-                throw new RuntimeException("Invalid browser name");
-
-        }
-        driver.manage().window().maximize();
-        driver.get(ConfigReader.getPropertyValue("url"));
-        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        openBrowserAndLaunchApplication();
 
 
     }
     @When("User Enters The correct UserName {string}")
     public void user_enters_the_correct_user_name(String username) {
+       sendText(login.usernameTextField,"username");
 
-        System.out.println(username);
+
     }
     @When("User Enters The correct Password {string}")
     public void user_enters_the_correct_password(String password) {
+        sendText(login.passwordTextField,"password");
 
-        System.out.println(password);
+
     }
     @When("user clicks On login button")
     public void user_clicks_on_login_button() {
+        login.loginButton.click();
 
-        System.out.println("4");
     }
     @Then("user is logged in")
     public void user_is_logged_in() {
 
-        System.out.println("5");
+        System.out.println("I am logged in");
     }
+
+
+    @Given("the user is navigated to the URl")
+    public void the_user_is_navigated_to_the_u_rl() {
+
+    }
+
+
+    @When("user enters username {string} and {string} verify the Error Message {string}")
+    public void user_enters_username_and_verify_the_error_message(String username, String password, String error) {
+      sendText(login.usernameTextField,username);
+      sendText(login.passwordTextField,password);
+      login.loginButton.click();
+      String errorMsg = login.errorMessage.getText();
+      Assert.assertEquals(error,errorMsg);
+    }
+
+    @When("user adds the employee")
+    public void user_adds_the_employee(DataTable dataTable){
+        List<Map<String, String>> employees = dataTable.asMaps();
+
+
+        for(Map<String,String>employee:employees){
+            String Fname=employee.get("firstName");
+            String mname=employee.get("middleName");
+            String lname=employee.get("lastName");
+            System.out.println(Fname);
+            System.out.println(mname);
+            System.out.println(lname);
+        }
+
+
+    }
+
 
 }
